86626fd9cbc9c3f99d8d559d451d78cfd9fb4b7e
mohamed adel <01200505348mohamed@gmail.com>
2024-04-19 19:15:39 +0200

    validation category

--- admin/category_add.php
+++ admin/category_add.php
@@ -1,27 +1,33 @@
 <?php
-    include("includes/header.php");
+include ("includes/header.php");
 ?>
 
-        <div class="container-fluid px-4 mt-4">
-            <form action="functions/process_book_edit.php" method="POST" enctype="multipart/form-data">
-                <div class=" mb-4">
-                    <div class="card-header">New category</div>
-                        <div class="card-body">
+<div class="container-fluid px-4 mt-4">
+    <form action="functions/process_category_add.php" method="POST" enctype="multipart/form-data">
+        <div class=" mb-4">
+            <div class="card-header">New category</div>
+            <div class="card-body">
 
-                            <div class="form-floating mb-3">
-                                <input name="category" type="text" class="form-control rounded-3" placeholder="Category Name">
-                                <label>Category Name</label>
-                            </div>
-
-                            <button type="submit" class="btn btn-primary btn-sm">Add Category</button>
-                            <a href="category_view.php" class="btn btn-primary btn-sm">Exit</a>
-
-                        </div>
-                    <div>
+                <div class="form-floating mb-3">
+                    <input name="category" type="text" class="form-control rounded-3" placeholder="Category Name">
+                    <label>Category Name</label>
+                    <div class="ms-3 text-danger"><?php if (isset($_SESSION['error']['category'])) {
+                        foreach ($_SESSION['error']['category'] as $error) {
+                            echo $error;
+                        }
+                    } ?></div>
                 </div>
-            </form>
-        </div>             
+
+                <button type="submit" class="btn btn-primary btn-sm">Add Category</button>
+                <a href="category_view.php" class="btn btn-primary btn-sm">Exit</a>
+
+            </div>
+            <div>
+            </div>
+    </form>
+</div>
 
 <?php
-    include("includes/footer.php");
+include ("includes/footer.php");
+unset($_SESSION['error']['category']);
 ?>
--- admin/functions/process_category_add.php
+++ admin/functions/process_category_add.php
@@ -1,34 +1,39 @@
 <?php
 
-	session_start();
+session_start();
 
-	if(!empty($_POST))
-	{
-		$_SESSION['error'] = array();
+if (!empty($_POST)) {
+	$_SESSION['error'] = array();
 
-		extract($_POST);
-
-		if(!empty($_SESSION['error']['category']))
-		{
-			header("location: category_add.php");
-			exit();
-		}
-		else
-		{
-			include("../includes/connection.php");
-
-			$query = "INSERT INTO `category_table`(`category_name`) VALUES ('$category')";
-
-			mysqli_query($connection_database, $query);
-
-			header("location: category_view.php");
-			exit();
-		}
-	}
-	else
-	{
-		header("location: category.php");
+	extract($_POST);
+	if (!$category) {
+		$_SESSION['error']['category']['required'] = "name category is required";
+		header("location:../category_add.php");
 		exit();
+	} else if (!preg_match("/^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/", $category)) {
+		$_SESSION['error']['category']['regex'] = "name category is invalid";
+		header("location:../category_add.php");
+		exit();
+	} else {
+		include ("../../includes/connection.php");
+
+		$query = "INSERT INTO `category_table`(`category_name`) VALUES ('$category')";
+
+		$result = mysqli_query($connection_database, $query);
+
+		if (!$result) {
+			$_SESSION['error']['category']['dont-add'] = "try again please";
+			header("location:../category_add.php");
+			exit();
+		} else {
+			header("location:../category_view.php");
+			exit();
+		}
 	}
+} else {
+	header("location:../../500.php");
+	exit();
+}
+
 
 ?>
